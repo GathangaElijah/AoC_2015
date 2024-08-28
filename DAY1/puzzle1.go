fd1f37a41075b692577e86d9561715b47af64fd7
egathang <elyg3672@gmail.com>
2024-08-28 11:56:53 +0300

    Feat: handle the basement position

--- DAY1/puzzle1.go
+++ DAY1/puzzle1.go
@@ -19,6 +19,29 @@ func FloorCalc(str string) int {
 
 	return floor
 }
+func Basement(str string) int{
+	floor := 0
+	var position int
+	for i := 0; i < len(str); i++{
+		bracket := string(str[i])
+		if bracket == "("{
+			floor ++
+			if floor == -1{
+				position = i +1
+				break
+			}
+		}
+		if bracket == ")"{
+			floor --
+			if floor == -1{
+				position = i +1
+				break
+			}
+		}
+		
+	}
+	return position
+}
 
 func main() {
 	str, err := os.ReadFile("input.txt")
@@ -28,4 +51,6 @@ func main() {
 	}else {
 		fmt.Println(err)
 	}
+	position := Basement(string(str))
+	fmt.Println(position)
 }
